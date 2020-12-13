a3df48767e39cb86842ff6a8269acbe54d4c6792
Alessandro Ramos <toprime13@gmail.com>
2020-12-13 20:39:04 -0300

    alterado output

--- src/componentes/Output.js
+++ src/componentes/Output.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './Output.css';
 
 class Output extends Component {
     render() {
+
+        var imcState = this.props.imcState
+
         return (
-            <div>
-                {this.props.IMC}
+            <div className='imc'>
+                <p>IMC: {imcState.toFixed(2)} </p>
             </div>
         );
     }
