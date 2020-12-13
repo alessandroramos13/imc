a0a6878d4c778ceb928b66da08a7a8551fb1ec07
Alessandro Ramos <toprime13@gmail.com>
2020-12-13 20:39:49 -0300

    update buttons

--- src/componentes/Buttons.js
+++ src/componentes/Buttons.js
@@ -4,31 +4,31 @@ import './Buttons.css';
 
 class Buttons extends Component {
 
-    constructor () {
+    constructor() {
         super()
         this.state = {
             peso: '',
             altura: '',
-            imc: null,
+            imc: 0,
         }
-        
+
     }
 
     setPeso = (e) => {
-        this.setState({peso: e.target.value})
+        this.setState({ peso: e.target.value })
     }
 
     setAltura = (e) => {
-        this.setState({altura: e.target.value})
+        this.setState({ altura: e.target.value })
     }
 
     setIMC = () => {
         let resultado = Number(this.state.peso) / (Number(this.state.altura) * Number(this.state.altura))
-        this.setState({peso: '', altura: '', imc: resultado})
+        this.setState({ peso: '', altura: '', imc: resultado })
         return (
             <Output />
         )
-    
+
     }
 
     render() {
@@ -36,12 +36,13 @@ class Buttons extends Component {
             return (
                 <div>
                     <div className='bt'>
-                        <input type="number" className='bt_peso' value={this.state.peso} placeholder={'Peso (em kg)'} onChange={this.setPeso}/>
-                        <input type="number" className='bt_altura' value={this.state.altura} placeholder={'Altura (em m)'} onChange={this.setAltura}/>
-                        <br/>
-                        <input type="submit" className='calc' value="Calcular" onClick={this.setIMC}/>
-                        <div className='imc'>
-                            <Output IMC={this.state.imc}/>
+                        <input type="number" className='bt_peso' value={this.state.peso} placeholder={'Peso (em kg)'} onChange={this.setPeso} />
+                        <input type="number" className='bt_altura' value={this.state.altura} placeholder={'Altura (em m)'} onChange={this.setAltura} />
+                        <br />
+                        <input type="submit" className='calc' value="Calcular" onClick={this.setIMC} />
+                        <div className='imcStyle'>
+                            <Output imcState={this.state.imc} />
+                            p
                         </div>
                     </div>
                 </div>
@@ -51,12 +52,12 @@ class Buttons extends Component {
             return (
                 <div>
                     <div className='bt'>
-                        <input type="number" className='bt_peso' value={this.state.peso} placeholder={'Peso (em kg)'} onChange={this.setPeso}/>
-                        <input type="number" className='bt_altura' value={this.state.altura} placeholder={'Altura (em m)'} onChange={this.setAltura}/>
-                        <br/>
-                        <input type="submit" className='calc' value="Calcular" disabled onClick={this.setIMC}/>
-                        <div className='imc'>
-                            <Output IMC={this.state.imc}/>
+                        <input type="number" className='bt_peso' value={this.state.peso} placeholder={'Peso (em kg)'} onChange={this.setPeso} />
+                        <input type="number" className='bt_altura' value={this.state.altura} placeholder={'Altura (em m)'} onChange={this.setAltura} />
+                        <br />
+                        <input type="submit" className='calc' value="Calcular" disabled onClick={this.setIMC} />
+                        <div className='imcStyle'>
+                            <Output imcState={this.state.imc} />
                         </div>
                     </div>
                 </div>
